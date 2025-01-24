b6d42aaf8827602a297a0182817b4934d830db09
DevynW <116035427+DevynW@users.noreply.github.com>
2025-01-24 14:04:21 -0500

    Doing the Homework
    Must move parent object to beads

---
+++ Assets/Scenes/beadRotator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beadRotator : MonoBehaviour
+{
+    [SerializeField] GameObject beadChain;
+    [SerializeField] float rotationFactor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rot = new Vector3 (beadChain.transform.rotation.x, beadChain.transform.rotation.y + 3, beadChain.transform.rotation.z);
+        beadChain.transform.Rotate(rot * Time.deltaTime);
+    }
+}
